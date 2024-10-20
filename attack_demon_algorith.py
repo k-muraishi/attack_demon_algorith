11d540b2aa13654b96d3e95c24032bf08066852a
“Kohei <muraishi634@gmail.com>
2024-10-20 18:43:04 +0900

    コメント追加

--- attack_demon_algorith.py
+++ attack_demon_algorith.py
@@ -6,6 +6,7 @@ def main():
     attack_point = int(lines[1])
     recovery_point = int(lines[2])
 
+    # 回復の方が多い場合倒せないので終了
     if attack_point <= recovery_point:
         print("NO")
         sys.exit(0)
@@ -13,6 +14,7 @@ def main():
     turn_count = 0
     demon_hp = int(lines[0])
 
+    # 攻撃した段階でhpが0以下なら終了
     while demon_hp > 0:
         demon_hp = attack_demon(demon_hp, attack_point)
         turn_count += 1
@@ -28,9 +30,11 @@ def get_stdin():
     input_values = input().split()
     return input_values
 
+# 魔王を攻撃
 def attack_demon(demon_hp, attack_point):
     return demon_hp - attack_point
 
+# 魔王を回復させる
 def recovery_demon(demon_hp, recovery_point):
     return demon_hp + recovery_point
 
